8f0dce6cf18eb197099804e2abf6fe62cc51dbed
AngeloLista <abstractplayer@gmail.com>
2022-02-10 20:16:39 +0100

    added header component

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header><img src="../assets/img/spotify-logo.png" alt="" /></header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped lang="scss">
+@import "../assets/scss/_vars.scss";
+
+header {
+  height: 70px;
+  background-color: $main_color;
+  color: white;
+  img {
+    height: 100%;
+    padding: 10px;
+  }
+}
+</style>
