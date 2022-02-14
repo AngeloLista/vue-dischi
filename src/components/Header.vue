c4a4cac834aa9da52769837213b0d556c0ef37ab
AngeloLista <abstractplayer@gmail.com>
2022-02-14 14:56:54 +0100

    added select component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,19 @@
 <template>
-  <header><img src="../assets/img/spotify-logo.png" alt="" /></header>
+  <header class="d-flex justify-content-between align-items-center">
+    <img src="../assets/img/spotify-logo.png" alt="" />
+    <div>
+      <Select />
+    </div>
+  </header>
 </template>
 
 <script>
+import Select from "./Select.vue";
 export default {
   name: "Header",
+  components: {
+    Select,
+  },
   props: ["genres"],
 };
 </script>
---
+++ src/components/Select.vue
@@ -0,0 +1,14 @@
+<template>
+  <select class="mx-3">
+    <option value="">Select an option</option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: "Select",
+};
+</script>
+
+<style>
+</style>
